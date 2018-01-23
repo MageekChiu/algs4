c3e771a2b2fbf166ff7188df22808fe4130f0654
queenk <qiukang@mail.ustc.edu.cn>
2018-01-23 21:55:26 +0800

    冒泡排序修改

--- src/main/java/edu/princeton/cs/algs4/BubbleSort.java
+++ src/main/java/edu/princeton/cs/algs4/BubbleSort.java
@@ -12,19 +12,23 @@ public class BubbleSort {
         for (Comparable anA : a) {
             StdOut.print(anA+"\t");
         }
-        StdOut.println();
+        StdOut.println("数组长度："+a.length);
     }
 
     private static void sort(Comparable[] a) throws IllegalAccessException, InstantiationException {
         Object tmp;
-        for (int i = 0;i<a.length-1;i++){
+        boolean noChange = false;
+        for (int i = 0;i<a.length-1 && !noChange;i++){
+            noChange = true;//如果某一趟没有交换，说明已经排好序无需再进行接下来的排序
             for (int j=0;j<a.length-1-i;j++){
                 if(a[j].compareTo(a[j+1])>0){
                     tmp =  a[j];
                     a[j] = a[j+1];
                     a[j+1] = (Comparable) tmp;
+                    noChange = false;//有交换
                 }
             }
+            System.out.println(noChange);
         }
     }
 
@@ -33,8 +37,9 @@ public class BubbleSort {
      */
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
 //        Integer[] a = {3,4,7,12,3,7,8,9,18,33,24,6,15,45,27};
-        Double[] a = {3.0,4.4,7.5,12.1,3.3,7.4,8.4,9.4,18.5,33.6,24.8,6.6,15.2,45.1,27.2};
-        StdRandom.shuffle(a);
+        Integer[] a = {3,4,7,9,12,13,16,20};
+//        Double[] a = {3.0,4.4,7.5,12.1,3.3,7.4,8.4,9.4,18.5,33.6,24.8,6.6,15.2,45.1,27.2};
+//        StdRandom.shuffle(a);
         show(a);
         sort(a);
         show(a);

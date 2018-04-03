6223bc1e62d3947ba42f27b3116904dc4a1df20f
queenk <qiukang@mail.ustc.edu.cn>
2018-04-03 19:47:47 +0800

    两个数的和查找

---
+++ src/main/java/edu/princeton/cs/other/Template.java
@@ -0,0 +1,25 @@
+package edu.princeton.cs.other;
+
+/**
+ *
+ * @author Mageek Chiu
+ * @date 2018/3/29 0029:12:21
+ */
+class Template {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int [] res = {0,0};
+        for (int i = 0;i<nums.length;i++){
+
+        }
+        return res;
+    }
+
+
+    public static void main (String ...args){
+       int[] nums = {2, 7, 11, 15}; int target = 9;
+       int[] res = twoSum(nums,target);
+       System.out.println(res[0]+","+res[1]);
+
+    }
+}

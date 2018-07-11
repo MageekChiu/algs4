bd5420e3ab1b15816104b275e67d0cfb02fd0ed3
queenk <qiukang@mail.ustc.edu.cn>
2018-07-11 17:03:52 +0800

    下一个排列

--- src/main/java/edu/princeton/cs/other/Template.java
+++ src/main/java/edu/princeton/cs/other/Template.java
@@ -2,20 +2,20 @@ package edu.princeton.cs.other;
 
 import static java.lang.System.out;
 /**
- *
+
  * @author Mageek Chiu
  */
 class Template {
 
     public static int maxSumSubArray(int[] nums) {
-        int len = nums.length,sum = 0,ans = Integer.MIN_VALUE;
+        int ans = 0;
 
         return ans;
     }
 
     // 感受：
     public static void main (String ...args){
-        int[] nums = {-2,2,-3,4,-1,2,1,-5,3};// 符合要求的子数组为[4,−1,2,1]，其最大和为6
+        int[] nums = {-2,2,-3,4,-1,2,1,-5,3};
         int res = maxSumSubArray(nums);//
         out.println(res);
     }

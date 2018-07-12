47063893a1eb479ef7e55e5a7e58e7a562c5102f
queenk <qiukang@mail.ustc.edu.cn>
2018-07-12 19:40:18 +0800

    搜索有序矩阵

--- src/main/java/edu/princeton/cs/other/SearchRange.java
+++ src/main/java/edu/princeton/cs/other/SearchRange.java
@@ -156,6 +156,105 @@ class SearchRange {
         return l;// l 就是待插入数字target在新数组中位置
     }
 
+    /**
+     给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
+     请注意，它是排序后的第k小元素，而不是第k个元素。
+
+     示例:
+     matrix = [
+        [ 1,  5,  9],
+        [10, 11, 13],
+        [12, 13, 15]
+     ],
+     k = 8,
+     返回 13。
+     说明:
+     你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2
+
+     思路1：首先 n 路归并排序 小到大，然后在找第k个，这样需要遍历所有元素
+
+
+     思路2：一个不熟悉的场景，对其特性与性质进行总结：
+     第i行（0开始）对角线处的元素[i,i]至少 >= （i+1）^2 个元素
+     左上方一定比当前元素小，
+     右下方一定比当前元素大，所以[i,i]  至少 <= (n-i)^2 个元素
+     升序排列第k个元素，亦即至多 < n-k 个元素（因为可能重复）
+
+     https://www.cnblogs.com/grandyang/p/5727892.html
+
+     思路3：一种利用堆的方法，我们使用一个最大堆，然后遍历数组每一个元素，将其加入堆，根据最大堆的性质，
+     大的元素会排到最前面，然后我们看当前堆中的元素个数是否大于k，大于的话就将首元素去掉，循环结束后我们返回堆中的首元素即为所求:
+
+     思路4：也可以用二分查找法来做，我们由于是有序矩阵，那么左上角的数字一定是最小的，而右下角的数字一定是最大的，
+     所以这个是我们搜索的范围，然后我们算出中间数字mid，由于矩阵中不同行之间的元素并不是严格有序的，
+     所以我们要在每一行都查找一下mid，我们使用upper_bound，这个函数是查找第一个大于目标数的元素，
+     如果目标数在比该行的尾元素大，则upper_bound返回该行元素的个数，如果目标数比该行首元素小，则upper_bound返回0,
+     我们遍历完所有的行可以找出中间数是第几小的数，然后k比较，进行二分查找，left和right最终会相等，并且会变成数组中第k小的数字。
+     举个例子来说吧，比如数组为:
+
+     [  1    2
+        12 100  ]
+     k = 3
+     那么刚开始left = 1, right = 100, mid = 50, 遍历完 cnt = 3，此时right更新为50
+     此时left = 1, right = 50, mid = 25, 遍历完之后 cnt = 3, 此时right更新为25
+     此时left = 1, right = 25, mid = 13, 遍历完之后 cnt = 3, 此时right更新为13
+     此时left = 1, right = 13, mid = 7, 遍历完之后 cnt = 2, 此时left更新为8
+     此时left = 8, right = 13, mid = 10, 遍历完之后 cnt = 2, 此时left更新为11
+     此时left = 11, right = 12, mid = 11, 遍历完之后 cnt = 2, 此时left更新为12
+     循环结束，left和right均为12，任意返回一个即可。
+
+
+     上面的解法还可以进一步优化到O(nlgX)，其中X为最大值和最小值的差值，我们并不用对每一行都做二分搜索法，
+     我们注意到每列也是有序的，我们可以利用这个性质，从数组的左下角开始查找，
+     如果比目标值小，我们就向右移一位，而且我们知道当前列的当前位置的上面所有的数字都小于目标值，
+     那么cnt += i+1，反之则向上移一位，这样我们也能算出cnt的值。其余部分跟上面的方法相同，
+
+     */
+    // O(nlgn*lgX)
+//    public static int kthSmallest(int[][] matrix, int k) {
+//        int n = matrix.length;
+//        int left = matrix[0][0], right = matrix[n-1][n-1];
+//        while (left < right) {
+//            int mid = left + (right - left) / 2, cnt = 0;
+//            for (int i = 0; i < matrix.size(); ++i) {
+//                cnt += upper_bound(matrix[i][0], matrix[i][n-1], mid) - matrix[i][0];
+//            }
+//            if (cnt < k) left = mid + 1;
+//            else right = mid;
+//        }
+//        return left;
+//    }
+//
+//    public static int upper_bound(int start,int end,int mid){
+//
+//    }
+
+
+    // O(nlgX)
+    public static int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0], right = matrix[n-1][n-1];
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt = search_less_equal(matrix, mid);
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+    public static int search_less_equal(int[][] matrix, int target) {
+        int n = matrix.length, i = n - 1, j = 0, res = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= target) {
+                res += i + 1;
+                ++j;
+            } else {
+                --i;
+            }
+        }
+        return res;
+    }
+
     // 感受：
     public static void main (String ...args){
 //        int[] nums = {5,7,7,8,8,10};
@@ -178,16 +277,16 @@ class SearchRange {
 //        int[] res = searchRange(nums,2);//[0,1]
 //        System.out.println(res[0]+","+res[1]);
 
-        int[] nums = {1,3,5,6};
-        System.out.println(searchInsert(nums,5));//2
-
-        int[] nums1 = {1,3,5,6};
-        System.out.println(searchInsert(nums1,2));//1
-
-        int[] nums2 = {2,3,5,6};
-        System.out.println(searchInsert(nums2,1));//0
-
-        int[] nums3 = {2,3,5,6};
-        System.out.println(searchInsert(nums3,7));//4
+//        int[] nums = {1,3,5,6};
+//        System.out.println(searchInsert(nums,5));//2
+//
+//        int[] nums1 = {1,3,5,6};
+//        System.out.println(searchInsert(nums1,2));//1
+//
+//        int[] nums2 = {2,3,5,6};
+//        System.out.println(searchInsert(nums2,1));//0
+//
+//        int[] nums3 = {2,3,5,6};
+//        System.out.println(searchInsert(nums3,7));//4
     }
 }

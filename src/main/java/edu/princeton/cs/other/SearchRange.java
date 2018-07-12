55e3ed4fd8467ba7fd8a3a3633f351f00c4e4e1b
queenk <qiukang@mail.ustc.edu.cn>
2018-07-12 14:47:03 +0800

    二分查找或插入

--- src/main/java/edu/princeton/cs/other/SearchRange.java
+++ src/main/java/edu/princeton/cs/other/SearchRange.java
@@ -121,6 +121,41 @@ class SearchRange {
 //            if(a[mid] <= dest)
 //    left = mid;
 
+    /**
+     给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     你可以假设数组中无重复元素。
+     示例 1:
+     输入: [1,3,5,6], 5
+     输出: 2
+     示例 2:
+     输入: [1,3,5,6], 2
+     输出: 1
+     示例 3:
+     输入: [1,3,5,6], 7
+     输出: 4
+     示例 4:
+     输入: [1,3,5,6], 0
+     输出: 0
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert(int[] nums, int target) {
+        int l = 0,r = nums.length-1,middle;
+        while (l<=r){
+            middle = l+(r-l)/2;
+            if (target==nums[middle]){
+                return middle;
+            }else if (target>nums[middle]){
+                l = middle+1;
+            }else {
+                r = middle-1;
+            }
+        }
+//        return r;// r 就是待插入数字target在新数组中的左边第一个位置
+        return l;// l 就是待插入数字target在新数组中位置
+    }
+
     // 感受：
     public static void main (String ...args){
 //        int[] nums = {5,7,7,8,8,10};
@@ -131,16 +166,28 @@ class SearchRange {
 //        int[] res1 = searchRange(nums1,6);//[-1,-1]
 //        out.println(res1[0]+","+res1[1]);
 
-        int[] nums1 = {5,7,7,8,8};
-        int[] res1 = searchRange(nums1,8);//[3,4]
-        System.out.println(res1[0]+","+res1[1]);
+//        int[] nums1 = {5,7,7,8,8};
+//        int[] res1 = searchRange(nums1,8);//[3,4]
+//        System.out.println(res1[0]+","+res1[1]);
 
 //        int[] nums = {0,0,5,7,7,8,8};
 //        int[] res = searchRange(nums,0);//[0,1]
 //        System.out.println(res[0]+","+res[1]);
 
-        int[] nums = {2,2};
-        int[] res = searchRange(nums,2);//[0,1]
-        System.out.println(res[0]+","+res[1]);
+//        int[] nums = {2,2};
+//        int[] res = searchRange(nums,2);//[0,1]
+//        System.out.println(res[0]+","+res[1]);
+
+        int[] nums = {1,3,5,6};
+        System.out.println(searchInsert(nums,5));//2
+
+        int[] nums1 = {1,3,5,6};
+        System.out.println(searchInsert(nums1,2));//1
+
+        int[] nums2 = {2,3,5,6};
+        System.out.println(searchInsert(nums2,1));//0
+
+        int[] nums3 = {2,3,5,6};
+        System.out.println(searchInsert(nums3,7));//4
     }
 }

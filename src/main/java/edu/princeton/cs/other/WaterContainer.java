a4cfdca72e8cd14424e3026ece4d810cf5b0e62a
queenk <qiukang@mail.ustc.edu.cn>
2018-07-14 17:41:47 +0800

    接雨水

--- src/main/java/edu/princeton/cs/other/WaterContainer.java
+++ src/main/java/edu/princeton/cs/other/WaterContainer.java
@@ -1,10 +1,12 @@
 package edu.princeton.cs.other;
 
+import static java.lang.Math.max;
 import static java.lang.System.out;
 
 /**
 
- 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。画 n 条垂直线，使得垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+ 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ 画 n 条垂直线，使得垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
  找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
 
  注意：你不能倾斜容器，n 至少是2。
@@ -41,7 +43,8 @@ class WaterContainer {
      双指针法
      算法
      这种方法背后的思路在于，两线段之间形成的区域总是会受到其中较短那条长度的限制。此外，两线段距离越远，得到的面积就越大。
-     我们在由线段长度构成的数组中使用两个指针，一个放在开始，一个置于末尾。 此外，我们会使用变量 maxareamaxarea 来持续存储到目前为止所获得的最大面积。
+     我们在由线段长度构成的数组中使用两个指针，一个放在开始，一个置于末尾。
+     此外，我们会使用变量 maxareamaxarea 来持续存储到目前为止所获得的最大面积。
      在每一步中，我们会找出指针所指向的两条线段形成的区域，更新 maxareamaxarea，并将指向较短线段的指针向较长线段那端移动一步。
 
      最初我们考虑由最外围两条线段构成的区域。现在，为了使面积最大化，我们需要考虑更长的两条线段之间的区域。
@@ -54,7 +57,7 @@ class WaterContainer {
     public static int maxArea(int[] height) {
         int result = 0, l = 0, r = height.length - 1;
         while (l < r) {
-            result = Math.max(result, Math.min(height[l], height[r]) * (r - l));
+            result = max(result, Math.min(height[l], height[r]) * (r - l));
             if (height[l] < height[r])// 移动较短的路径
                 l++;
             else
@@ -65,8 +68,43 @@ class WaterContainer {
 
     // 感受：
     public static void main (String ...args){
-        int[] nums = {3,5,8,4,2,6};//
-        int res = maxArea(nums);//
-        out.println(res);
+//        int[] nums = {3,5,8,4,2,6};//
+//        int res = maxArea(nums);//
+//        out.println(res);
+
+        int[] nums1 = {0,1,0,2,1,0,1,3,2,1,2,1};//6
+        out.println(trap(nums1));
+    }
+
+    /**
+     给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+     上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+     示例:
+     输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+     输出: 6
+
+     思路1: 本质就是找波峰，水量是波峰之间的容量，可以标记一下左右波峰，亦即一开始左波峰，下一个就是右，在下一个又是左
+     思路2：双指针
+     木桶原理，盛的水取决于最短板，我们设置两个指针从左右两端开始向中间靠拢，我们根据短的那一端向高的那一端靠拢。
+     然后过程中更新左右最高的柱子，来求的中间的蓄水量。
+     */
+    public static int trap(int[] height) {
+        int res = 0;
+        int l = 0, r = height.length - 1;
+        int lmax = 0, rmax = 0;
+        while(l < r){
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+            if (lmax < rmax){
+                res += lmax - height[l];
+                l++;
+            }else{
+                res += rmax - height[r];
+                r--;
+            }
+        }
+        return res;
+
     }
 }

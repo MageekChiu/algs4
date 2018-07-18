5478c3710154874b0f97c0edfa358512c9b52d86
queenk <qiukang@mail.ustc.edu.cn>
2018-07-18 18:40:38 +0800

    数组去重

--- src/main/java/edu/princeton/cs/other/DeDuplicateArray.java
+++ src/main/java/edu/princeton/cs/other/DeDuplicateArray.java
@@ -1,47 +1,52 @@
 package edu.princeton.cs.other;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static java.lang.System.currentTimeMillis;
 import static java.lang.System.out;
 
 /**
  数组去重
  给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
-
  不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
-
  示例 1:
-
  给定数组 nums = [1,1,2],
-
  函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
-
  你不需要考虑数组中超出新长度后面的元素。
  示例 2:
-
  给定 nums = [0,0,1,1,1,2,2,3,3,4],
-
  函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
-
  你不需要考虑数组中超出新长度后面的元素。
  说明:
-
  为什么返回数值是整数，但输出的答案是数组呢?
-
  请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
-
  你可以想象内部操作如下:
-
  // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
  int len = removeDuplicates(nums);
-
  // 在函数里修改输入数组对于调用者是可见的。
  // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
  for (int i = 0; i < len; i++) {
     print(nums[i]);
+ }
+
+ ------------------------------------------------------------------------------------------------------------
+ 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+ 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ 示例 1:
+ 给定 nums = [1,1,1,2,2,3],
+ 函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
+ 你不需要考虑数组中超出新长度后面的元素。
+ 示例 2:
+ 给定 nums = [0,0,1,1,1,1,2,3,3],
+ 函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
+ 你不需要考虑数组中超出新长度后面的元素。
+ 说明:
+ 为什么返回数值是整数，但输出的答案是数组呢?
+ 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ 你可以想象内部操作如下:
+ // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ int len = removeDuplicates(nums);
+ // 在函数里修改输入数组对于调用者是可见的。
+ // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ for (int i = 0; i < len; i++) {
+ print(nums[i]);
  }
  * @author Mageek Chiu
  */
@@ -60,20 +65,48 @@ class DeDuplicateArray {
         if(A.length==0||A.length==1){
             return A.length;
         }
+        while(i<A.length&&j<A.length){//直接把不重复的数拿出来即可
+            if (A[i]!=A[j]){
+                A[++i] = A[j];
+            }
+            j++;
+        }
+        return i+1;
+    }
+
+    /**
+     * 在上一题的基础之上加一个判断即可
+     * @param A
+     * @return
+     */
+    public static int removeDuplicates2(int[] A) {
+        int i=0;
+        int j=1;
+        if(A.length==0||A.length==1) return A.length;
+        int duplicate = 0;
         while(i<A.length&&j<A.length){
             if(A[i]==A[j]){
+                duplicate++;
+                if (duplicate==1){
+                    A[++i]=A[j];
+                }
                 j++;
             }else{
+                duplicate = 0;
                 A[++i]=A[j++];
             }
-
         }
         return i+1;
     }
 
     // 感受：
     public static void main (String ...args){
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};//
-        out.println(removeDuplicates(nums));
+//        int[] nums = {0,0,1,1,1,2,2,3,3,4};//
+        int[] nums = {1,1,1,2,2,3};//
+//        out.println(removeDuplicates(nums));
+        out.println(removeDuplicates2(nums));
+        for (int num : nums) {
+            out.print(num+",");
+        }
     }
 }

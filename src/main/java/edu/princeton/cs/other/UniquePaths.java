101916b5e7d440cc5e66aca2482927d5ab81d4df
queenk <qiukang@mail.ustc.edu.cn>
2018-07-19 13:33:50 +0800

    最小路径和

--- src/main/java/edu/princeton/cs/other/UniquePaths.java
+++ src/main/java/edu/princeton/cs/other/UniquePaths.java
@@ -45,6 +45,21 @@ import static java.lang.System.out;
 
  这个问题就只能用DP了，如果障碍点只有1,2个依然可以通过排除法转换为组合数问题，但是这里障碍物数量未知
 
+----------------------------------------------------------
+
+ 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ 说明：每次只能向下或者向右移动一步。
+ 示例:
+ 输入:
+ [
+ [1,3,1],
+ [1,5,1],
+ [4,2,1]
+ ]
+ 输出: 7
+ 解释: 因为路径 1→3→1→1→1 的总和最小。
+
+ 思路，标准的DP问题
 
  * @author Mageek Chiu
  */
@@ -112,6 +127,22 @@ class UniquePaths {
         return path[m-1][n-1];
     }
 
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        if (m<1) return 0;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if (i==0 && j>0) dp[i][j] =dp[i][j-1]+ grid[i][j];// 行起始条件
+                if (j==0 && i>0) dp[i][j] =dp[i-1][j]+ grid[i][j];// 列起始条件
+                if (i>0 && j>0) dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     public static int[][] C = null;
     public static int rows = 200,cols = 200;
 
@@ -147,7 +178,13 @@ class UniquePaths {
 
     // 感受：
     public static void main (String ...args){
-        out.println(uniquePaths(7,3));//28
-        out.println(uniquePaths(3,2));//3
+//        out.println(uniquePaths(7,3));//28
+//        out.println(uniquePaths(3,2));//3
+
+        out.println(minPathSum(new int[][]{
+                {1,3,1},
+                {1,5,1},
+                {4,2,1}
+        }));
     }
 }
